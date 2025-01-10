3ebb769d81ce4131e98e69f1c580557b51f15899
Kevin Hernandez <kevin.hernandez288@gmail.com>
2025-01-10 15:21:02 -0800

    Added chadcn button

--- builder-registry.ts
+++ builder-registry.ts
@@ -1,5 +1,6 @@
 "use client";
-import { builder, Builder } from "@builder.io/react";
+import { builder, Builder, withChildren } from "@builder.io/react";
+import { Button } from "./components/ui/button";
 import Counter from "./components/Counter/Counter";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
@@ -10,6 +11,32 @@ Builder.registerComponent(Counter, {
     {
       name: "initialCount",
       type: "number",
+    },
+  ],
+});
+
+Builder.registerComponent(withChildren(Button), {
+  name: "Button",
+  inputs: [
+    {
+      name: "children",
+      type: "string",
+      hideFromUI: true,
+      meta: {
+        ts: "ReactNode",
+      },
+    },
+    {
+      name: "variant",
+      type: "string",
+      enum: ["default", "destructive", "outline", "secondary", "ghost", "link"],
+      defaultValue: "default",
+    },
+    {
+      name: "size",
+      type: "string",
+      enum: ["default", "sm", "lg", "icon"],
+      defaultValue: "default",
     },
   ],
 });
