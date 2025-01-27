4b95c7f229465255e1f0cfda08ca8eb2e301a892
Kevin Hernandez <kevin.hernandez288@gmail.com>
2025-01-27 12:04:17 -0800

    Added JSDoc to NavbarProps

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 
+/**
+ * A component that renders a navbar with a shadow
+ * @param {React.ReactNode} children - The children to render
+ * @param {string} className - The class name to add to the navbar
+ * @param {string} shadow - The shadow to add to the navbar
+ * @param {number} padding - The padding around the navbar
+ */
 interface NavbarProps {
   children: React.ReactNode;
   className?: string;
